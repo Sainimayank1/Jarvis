ed968224a2778b48f34b8cc3b7f5c5a1b7e4d7da
Mayank saini <mayanksaini4455@gmail.com>
2023-09-01 12:20:21 +0530

    Done with homeScreen

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import {View, Text, Image, ScrollView} from 'react-native';
+import {View, Text, Image, ScrollView, TouchableOpacity} from 'react-native';
 import React, {useState} from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {
@@ -10,6 +10,9 @@ import {dummyMessages} from '../Constants';
 
 export default function HomeScreen() {
   const [messages, setMessages] = useState(dummyMessages);
+  const [recording, setRecording] = useState(false);
+  const [speaking, setSpeaking] = useState(true);
+
   return (
     <SafeAreaView className="flex-1 bg-white">
       <View className="flex flex-1 mx-5">
@@ -38,20 +41,24 @@ export default function HomeScreen() {
                 {messages.map((item, index) => {
                   if (item.role == 'assistent') {
                     if (item.content.includes('https')) {
-                        return <View
+                      return (
+                        <View
+                          key={index}
                           style={{width: wp(80)}}
                           className="flex-row justify-start">
                           <View className="bg-emerald-200 p-2 rounded-xl rounded-tl-none">
                             <Image
-                            source={{uri:item.content}}
-                            resizeMode='contain'
-                            style={{width:wp(60),height:wp(60)}}
-                            className="rouned-2xl"></Image>
+                              source={{uri: item.content}}
+                              resizeMode="contain"
+                              style={{width: wp(60), height: wp(60)}}
+                              className="rouned-2xl"></Image>
                           </View>
                         </View>
+                      );
                     } else {
                       return (
                         <View
+                          key={index}
                           style={{width: wp(80)}}
                           className="flex-row justify-start">
                           <View className="bg-emerald-200 p-2 rounded-xl rounded-tl-none">
@@ -61,8 +68,9 @@ export default function HomeScreen() {
                       );
                     }
                   } else {
-                    return ( 
+                    return (
                       <View
+                        key={index}
                         style={{width: wp(80)}}
                         className="flex-row justify-end">
                         <View className="bg-white p-2 rounded-xl rounded-tr-none">
@@ -78,6 +86,43 @@ export default function HomeScreen() {
         ) : (
           <Features />
         )}
+
+        {/* Recoding button */}
+        <View className="flex justify-center items-center">
+          {recording ? (
+            <TouchableOpacity>
+              <Image
+                source={require('../../assets/images/voiceLoading.gif')}
+                className="rounded-full"
+                style={{height: hp(10), width: hp(10)}}></Image>
+            </TouchableOpacity>
+          ) : (
+            <TouchableOpacity onPress={() => setRecording(!recording)}>
+              <Image
+                source={require('../../assets/images/recordingIcon.png')}
+                className="rounded-full"
+                style={{height: hp(10), width: hp(10)}}></Image>
+            </TouchableOpacity>
+          )}
+
+          {/* Clear Button */}
+          {messages.length > 0 && (
+            <TouchableOpacity
+              onPress={() => setMessages([])}
+              className="bg-neutral-400 rounded-3xl p-2 absolute right-10">
+              <Text className="text-white font-semibold">Clear</Text>
+            </TouchableOpacity>
+          )}
+
+          {/* Stop Button */}
+          {recording && (
+            <TouchableOpacity
+              onPress={() => setRecording(!recording)}
+              className="bg-red-400 rounded-3xl p-2 absolute left-10">
+              <Text className="text-white font-semibold">Stop</Text>
+            </TouchableOpacity>
+          )}
+        </View>
       </View>
     </SafeAreaView>
   );
